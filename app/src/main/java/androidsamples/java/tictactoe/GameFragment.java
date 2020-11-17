7247b760077494f6888ecbef76c2e75c8f8dbdfb
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2020-11-17 02:58:37 -0700

    Implemented: game grid, handling back button to forfeit the game

--- app/src/main/java/androidsamples/java/tictactoe/GameFragment.java
+++ app/src/main/java/androidsamples/java/tictactoe/GameFragment.java
@@ -1,19 +1,29 @@
 package androidsamples.java.tictactoe;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class GameFragment extends Fragment {
-    private static final String TAG = "GameFragment";
     public static final int ONE_PLAYER_GAME = 1;
     public static final int TWO_PLAYER_GAME = 2;
+
+    private static final String TAG = "GameFragment";
+    private static final int GRID_SIZE = 9;
+
+    private final Button[] mButtons = new Button[GRID_SIZE];
+    private NavController mNavController;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +31,27 @@ public class GameFragment extends Fragment {
 
         GameFragmentArgs args = GameFragmentArgs.fromBundle(getArguments());
         Log.d(TAG, "New game type = " + args.getGameType());
+
+        // Handles the back press by adding a confirmation to forfeit the game
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                Log.d(TAG, "Back pressed");
+
+                // TODO show dialog only when the game is still in progress
+                AlertDialog dialog = new AlertDialog.Builder(requireActivity())
+                        .setTitle("Confirm")
+                        .setMessage("Are you sure you want to forfeit this game?")
+                        .setPositiveButton("Yes", (d, which) -> {
+                            // TODO update loss count
+                            mNavController.popBackStack();
+                        })
+                        .setNegativeButton("Cancel", (d, which) -> d.dismiss())
+                        .create();
+                dialog.show();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
     }
 
     @Override
@@ -30,9 +61,30 @@ public class GameFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // TODO logic for button clicks
+        mNavController = Navigation.findNavController(view);
+
+        mButtons[0] = view.findViewById(R.id.button0);
+        mButtons[1] = view.findViewById(R.id.button1);
+        mButtons[2] = view.findViewById(R.id.button2);
+
+        mButtons[3] = view.findViewById(R.id.button3);
+        mButtons[4] = view.findViewById(R.id.button4);
+        mButtons[5] = view.findViewById(R.id.button5);
+
+        mButtons[6] = view.findViewById(R.id.button6);
+        mButtons[7] = view.findViewById(R.id.button7);
+        mButtons[8] = view.findViewById(R.id.button8);
+
+        for (int i = 0; i < mButtons.length; i++) {
+            int finalI = i;
+            mButtons[i].setOnClickListener(v -> {
+                Log.d(TAG, "Button " + finalI + " clicked");
+                // TODO implement listeners
+            });
+        }
     }
 }
